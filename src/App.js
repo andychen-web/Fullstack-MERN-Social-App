dfa5596547c1b30ebaf3c29f94639f4b3b809487
andychen <descartes1960s@gmail.com>
2023-02-25 10:53:55 +0800

    create react folder structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profile page";
+
 function App() {
   return (
     <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={LoginPage} />
+          <Route path="/home" element={HomePage} />
+          <Route path="/profilePage/:userId" element={ProfilePage} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
