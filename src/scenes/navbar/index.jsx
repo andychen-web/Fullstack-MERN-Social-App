100d2405fcf7ad74a4f1994aaeb6b5c938f69955
andychen <descartes1960s@gmail.com>
2023-03-03 11:37:31 +0800

    import icons from MUI library

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -1,3 +1,16 @@
+import { useState } from "react";
+
+import {
+  Search,
+  Message,
+  Notifications,
+  Help,
+  Menu,
+  Close,
+  DarkMode,
+  LightMode,
+} from "@mui/icons-material";
+
 const Navbar = () => {
   return <div></div>;
 };
