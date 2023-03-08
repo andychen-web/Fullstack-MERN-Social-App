ecf482f0ff8d02b8177108fc2013a07f0c1c31b2
andychen <descartes1960s@gmail.com>
2023-03-08 16:13:40 +0800

    complete menu feature

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -1,19 +1,37 @@
 import { useState } from "react";
 
-import { Search, Notifications, Help } from "@mui/icons-material";
 import {
+  Search,
+  Message,
+  Notifications,
+  Help,
+  MoreVert,
+} from "@mui/icons-material";
+
+import {
+  IconButton,
   AppBar,
   Toolbar,
   Typography,
   TextField,
   InputAdornment,
+  Box,
+  Menu,
+  MenuItem,
 } from "@mui/material";
 
 const Navbar = () => {
   const black = "#292e36";
+  const beige = "#F3F3F3";
+
+  const [menuOpen, setMenuOpen] = useState(false);
+  const handleUserClick = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <>
-      <AppBar>
+      <AppBar sx={{ overflow: "hidden", background: "white" }}>
         {/* Mobile Navbar */}
 
         {/* Desktop Navbar */}
@@ -22,9 +40,11 @@ const Navbar = () => {
             bgcolor: "white",
             display: "flex",
             justifyContent: "space-between",
+            flexWrap: "nowrap",
           }}
         >
-          <>
+          {/* LOGO and Search Bar */}
+          <Box sx={{ display: "flex", alignItems: "center" }}>
             <Typography
               variant="h6"
               sx={{ color: "#42a5f5" }}
@@ -33,7 +53,14 @@ const Navbar = () => {
               FriendSphere
             </Typography>
             <TextField
-              sx={{ margin: ".5rem", fontSize: "1.5rem" }}
+              sx={{
+                margin: ".5rem",
+                fontSize: "1rem",
+                borderRadius: ".5rem",
+                overflow: "hidden",
+                background: beige,
+              }}
+              variant="standard"
               InputProps={{
                 endAdornment: (
                   <InputAdornment position="end">
@@ -42,9 +69,45 @@ const Navbar = () => {
                 ),
               }}
             />
-          </>
-          <Notifications sx={{ fontSize: "1.5rem", color: black }} />
-          <Help sx={{ fontSize: "1.5rem", color: black }} />
+          </Box>
+          {/* Nav Items */}
+          <Box sx={{ display: "flex" }}>
+            <Message
+              sx={{ fontSize: "1.5rem", color: black, paddingTop: "2px" }}
+            />
+            <Notifications
+              sx={{ fontSize: "1.5rem", color: black, paddingX: ".7rem" }}
+            />
+            <Help sx={{ fontSize: "1.5rem", color: black }} />
+
+            <IconButton onClick={handleUserClick}>
+              <MoreVert sx={{ fontSize: "1.5rem", color: "black" }} />
+            </IconButton>
+            <Menu
+              sx={{
+                width: "150px",
+                position: "absolute",
+                right: "0",
+                top: "0",
+                left: "unset",
+                transform: "translateX(90vw)",
+                transform: "translateY(-63vh)",
+              }}
+              id="menu"
+              open={menuOpen}
+              onClose={handleUserClick}
+            >
+              <MenuItem sx={{ fontSize: ".8rem" }} onClick={handleUserClick}>
+                User
+              </MenuItem>
+              <MenuItem sx={{ fontSize: ".8rem" }} onClick={handleUserClick}>
+                My account
+              </MenuItem>
+              <MenuItem sx={{ fontSize: ".8rem" }} onClick={handleUserClick}>
+                Logout
+              </MenuItem>
+            </Menu>
+          </Box>
         </Toolbar>
       </AppBar>
     </>
