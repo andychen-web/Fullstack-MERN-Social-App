f6c0c5950bcf4ab739d36e1643cb6364b717a79d
andychen <descartes1960s@gmail.com>
2023-03-14 09:48:11 +0800

    set up profilePage

--- src/scenes/loginPage/index.jsx
+++ src/scenes/loginPage/index.jsx
@@ -1,8 +1,36 @@
 import useState from "react";
-import { useMediaQuery } from "@mui/material";
+import { useMediaQuery, Box, Button, Form } from "@mui/material";
 
 const LoginPage = () => {
-  return <></>;
+  const beige = "#F3F3F3";
+
+  return (
+    <Box bgcolor={beige}>
+      <Box
+        display="flex"
+        justifyContent="center"
+        width="100vw"
+        height="100vh"
+        pt="100px"
+      >
+        <Box
+          textAlign="center"
+          maxWidth="80%"
+          bgcolor="white"
+          width="50vw"
+          height="50vh"
+          fontSize=".8rem"
+        >
+          <span>
+            Welcome to FriendSphere, where making friends is so easy!{" "}
+          </span>
+          <input type="text" placeholder="Email" />
+          <input type="text" placeholder="Password" />
+          <Button variant="standard"> Login</Button>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default LoginPage;
