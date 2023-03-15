4e0fcacbcc794a67744e1ba68fa58cfd10335c6a
andychen <descartes1960s@gmail.com>
2023-03-15 15:31:38 +0800

    Improve UI appearance

--- src/scenes/loginPage/Form.jsx
+++ src/scenes/loginPage/Form.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import { Box, FormControl, Button, Input } from "@mui/material";
 
-const Form = () => {
+const Form = ({ isLargeScreen }) => {
   return (
     <Box display="flex" flexDirection="column" px="2rem">
       {/* use "& childElement" syntax in MUI to select child elements */}
-      <FormControl sx={{ "& input": { pt: ".8rem" } }}>
-        <Input type="text" sx={{ outline: "none" }} placeholder="Email" />
-        <Input type="text" sx={{ outline: "none" }} placeholder="Password" />
+      <FormControl sx={{ "& input": { py: ".5rem" } }}>
+        <Input
+          type="text"
+          disableUnderline={true}
+          sx={{
+            outline: "1px solid lightgrey",
+            borderRadius: "5px",
+            my: "1rem",
+          }}
+          placeholder=" Email"
+        />
+        <Input
+          type="text"
+          disableUnderline={true}
+          sx={{ outline: "1px solid lightgrey", borderRadius: "5px" }}
+          placeholder=" Password"
+        />
       </FormControl>
-      <Button variant="contained" sx={{ mt: ".5rem", bgcolor: "#42a5f5" }}>
+      <Button variant="contained" sx={{ mt: "1rem", bgcolor: "#42a5f5" }}>
         Login
       </Button>
     </Box>
