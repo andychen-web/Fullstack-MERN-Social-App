65e265ddca903f30f1ca96f1d574e46522cd3cec
andychen <descartes1960s@gmail.com>
2023-03-15 11:21:09 +0800

    Pass RWD design to components

--- src/App.js
+++ src/App.js
@@ -3,15 +3,28 @@ import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
 import Navbar from "scenes/navbar";
+import useMediaQuery from "@mui/material/useMediaQuery";
+
 function App() {
+  const isLargeScreen = useMediaQuery("(min-width: 700px)");
+
   return (
     <div className="app">
       <BrowserRouter>
-        <Navbar />
+        <Navbar isLargeScreen={isLargeScreen} />
         <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profilePage/:userId" element={<ProfilePage />} />
+          <Route
+            path="/"
+            element={<LoginPage isLargeScreen={isLargeScreen} />}
+          />
+          <Route
+            path="/home"
+            element={<HomePage isLargeScreen={isLargeScreen} />}
+          />
+          <Route
+            path="/profilePage/:userId"
+            element={<ProfilePage isLargeScreen={isLargeScreen} />}
+          />
         </Routes>
       </BrowserRouter>
     </div>
