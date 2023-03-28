189ae537b032d2a1f471d4a843aa4fece1ce1071
andychen <descartes1960s@gmail.com>
2023-03-28 14:14:46 +0800

    Modify navbar for RWD

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -41,181 +41,110 @@ const Navbar = ({ isLargeScreen }) => {
           bgcolor: "white",
         }}
       >
-        {isLargeScreen ? (
-          <Toolbar
-            sx={{
-              bgcolor: "white",
-              justifyContent: "space-between",
-              width: "95%",
-            }}
-          >
-            {/* Large Screen: LOGO and Search Bar */}
-            <Box display="flex" alignItems="center" width="90%">
-              <Typography variant="h6" color="#42a5f5">
-                FriendSphere
-              </Typography>
-              <TextField
-                sx={{
-                  margin: ".5rem",
-                  fontSize: "1rem",
-                  borderRadius: ".5rem",
-                  overflow: "hidden",
-                  background: beige,
-                }}
-                variant="standard"
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <Search
-                        sx={{ color: "black", cursor: "pointer" }}
-                        onClick={() => {
-                          alert("do search feature");
-                        }}
-                      />
-                    </InputAdornment>
-                  ),
-                }}
-              />
-            </Box>
-            {/* Nav Items */}
-            <Box
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "end",
-              }}
+        <Toolbar
+          sx={{
+            bgcolor: "white",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <Box sx={{ display: "flex", alignItems: "center", width: "90%" }}>
+            <Typography
+              variant={isLargeScreen ? "h6" : "caption"}
+              fontWeight="500"
+              color="#42a5f5"
             >
-              <Message
-                sx={{
-                  fontSize: "1.5rem",
-                  color: black,
-                  paddingTop: "2px",
-                  cursor: "pointer",
-                }}
-              />
+              FriendSphere
+            </Typography>
+            <TextField
+              sx={{
+                margin: ".5rem",
+                borderRadius: ".5rem",
+                overflow: "hidden",
+                background: beige,
+              }}
+              variant="standard"
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <Search
+                      sx={{
+                        fontSize: isLargeScreen ? "1rem" : ".9rem",
+                        color: black,
+                        cursor: "pointer",
+                      }}
+                    />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Box>
+          {/* Nav Items */}
+          <Box display="flex" alignItems="center">
+            <Message
+              sx={{
+                fontSize: isLargeScreen ? "1.5rem" : "1rem",
+                color: black,
+                paddingTop: "2px",
+                cursor: "pointer",
+              }}
+            />
+            <Notifications
+              sx={{
+                fontSize: isLargeScreen ? "1.5rem" : "1rem",
+                color: black,
+                paddingX: ".7rem",
+                cursor: "pointer",
+              }}
+            />
+            <Help
+              sx={{
+                fontSize: isLargeScreen ? "1.5rem" : "1rem",
+                color: black,
+                cursor: "pointer",
+              }}
+            />
 
-              <Notifications
+            <IconButton onClick={handleUserClick}>
+              <MoreVert
                 sx={{
-                  fontSize: "1.5rem",
-                  color: black,
-                  paddingX: ".7rem",
-                  cursor: "pointer",
+                  fontSize: isLargeScreen ? "1.5rem" : "1rem",
+                  color: "black",
                 }}
               />
-              <Help
-                sx={{ fontSize: "1.5rem", color: black, cursor: "pointer" }}
-              />
-
-              <IconButton onClick={handleUserClick}>
-                <MoreVert sx={{ fontSize: "1.5rem", color: "black" }} />
-              </IconButton>
-              <Menu
-                sx={{
-                  width: "150px",
-                  minHeight: "190px",
-                  position: "absolute",
-                  marginY: "50px",
-                  right: "0",
-                  left: "unset",
-                  top: "-30vh",
-                  transform: "translateX(90vw)",
-                  transform: "translateY(-40vh)",
-                }}
-                id="menu"
-                open={menuOpen}
-                onClose={handleUserClick}
-              >
-                <MenuItem value={fullName} onClick={handleUserClick}>
-                  <Typography sx={{ fontSize: ".8rem" }}>{fullName}</Typography>
-                </MenuItem>
-                <MenuItem sx={{ fontSize: ".8rem" }} onClick={handleUserClick}>
-                  Logout
-                </MenuItem>
-              </Menu>
-            </Box>
-          </Toolbar>
-        ) : (
-          <Toolbar
-            sx={{
-              bgcolor: "white",
-              display: "flex",
-              justifyContent: "space-between",
-            }}
-          >
-            {/* Smaller Screen: LOGO and Search Bar */}
-            <Box sx={{ display: "flex", alignItems: "center", width: "90%" }}>
-              <Typography fontWeight="500" color="#42a5f5">
-                FriendSphere
-              </Typography>
-              <TextField
-                sx={{
-                  margin: ".5rem",
-                  fontSize: "1rem",
-                  borderRadius: ".5rem",
-                  overflow: "hidden",
-                  background: beige,
-                }}
-                variant="standard"
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <Search sx={{ color: black, cursor: "pointer" }} />
-                    </InputAdornment>
-                  ),
-                }}
-              />
-            </Box>
-            {/* Nav Items */}
-            <Box display="flex" alignItems="center">
-              <Message
-                sx={{
-                  fontSize: "1rem",
-                  color: black,
-                  paddingTop: "2px",
-                  cursor: "pointer",
-                }}
-              />
-              <Notifications
-                sx={{
-                  fontSize: "1rem",
-                  color: black,
-                  paddingX: ".7rem",
-                  cursor: "pointer",
-                }}
-              />
-              <Help
-                sx={{ fontSize: "1rem", color: black, cursor: "pointer" }}
-              />
-
-              <IconButton onClick={handleUserClick}>
-                <MoreVert sx={{ fontSize: "1rem", color: "black" }} />
-              </IconButton>
-              <Menu
-                sx={{
-                  width: "150px",
-                  minHeight: "190px",
-                  position: "absolute",
-                  marginY: "50px",
-                  right: "0",
-                  left: "unset",
-                  top: "-48vh",
-                  transform: "translateX(0)",
-                  transform: "translateY(-40vh)",
-                }}
-                id="menu"
-                open={menuOpen}
-                onClose={handleUserClick}
-              >
-                <MenuItem value={fullName} onClick={handleUserClick}>
-                  <Typography sx={{ fontSize: ".8rem" }}>{fullName}</Typography>
-                </MenuItem>
-                <MenuItem sx={{ fontSize: ".8rem" }} onClick={handleUserClick}>
-                  Logout
-                </MenuItem>
-              </Menu>
-            </Box>
-          </Toolbar>
-        )}
+            </IconButton>
+            <Menu
+              sx={{
+                width: "150px",
+                minHeight: "190px",
+                position: "absolute",
+                marginY: "50px",
+                right: "0",
+                left: "unset",
+                top: "-48vh",
+                transform: isLargeScreen
+                  ? "translate(0,-25vh)"
+                  : "translate(0,-35vh)",
+              }}
+              id="menu"
+              open={menuOpen}
+              onClose={handleUserClick}
+            >
+              <MenuItem value={fullName} onClick={handleUserClick}>
+                <Typography
+                  sx={{
+                    fontSize: ".8rem",
+                  }}
+                >
+                  {fullName}
+                </Typography>
+              </MenuItem>
+              <MenuItem sx={{ fontSize: ".8rem" }} onClick={handleUserClick}>
+                Logout
+              </MenuItem>
+            </Menu>
+          </Box>
+        </Toolbar>
       </AppBar>
     </>
   );
