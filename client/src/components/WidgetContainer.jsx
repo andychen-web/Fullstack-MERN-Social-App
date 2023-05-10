b7bcb7ff8471a05f46ee2e6a3ecfcb359559a305
andychen <descartes1960s@gmail.com>
2023-05-10 15:17:00 +0800

    chore: shrink widget container size for better UI

--- client/src/components/WidgetContainer.jsx
+++ client/src/components/WidgetContainer.jsx
@@ -9,7 +9,8 @@ const WidgetContainer = ({ children }) => {
     <Box
       width={isLargeScreen ? "25vw" : "65vw"}
       sx={{
-        maxHeight: "85vh",
+        height: "auto",
+        maxHeight: "300px",
         padding: "1rem",
         paddingBottom: ".5rem",
         backgroundColor: "white",
