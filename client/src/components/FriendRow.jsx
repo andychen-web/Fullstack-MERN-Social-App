960bbd96f57be0b5ae921e9485784a4bbb4b70e2
Andy-Chen <79246459+andychen-web@users.noreply.github.com>
2023-05-14 20:23:41 +0800

    test: Investigate and debug friendList issue
    friendList works well on localhost, fail on the deployed website

--- client/src/components/FriendRow.jsx
+++ client/src/components/FriendRow.jsx
@@ -12,50 +12,50 @@ const FriendRow = () => {
   const token = useSelector((state) => state.token);
   const userId = useSelector((state) => state.user._id);
 
-  const addRemoveFriend = async (friendID) => {
-    try {
-      const response = await fetch(
-        `https://social-app-backend-3j7e.onrender.com/users/${userId}/${friendID}`,
-        {
-          method: "PATCH",
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      const data = await response.json();
-      dispatch(setFriends({ friends: data }));
-    } catch (error) {
-      console.error("Error adding friend:", error);
-    }
-  };
+//   const addRemoveFriend = async (friendID) => {
+//     try {
+//       const response = await fetch(
+//         `https://social-app-backend-3j7e.onrender.com/users/${userId}/${friendID}`,
+//         {
+//           method: "PATCH",
+//           headers: {
+//             Authorization: `Bearer ${token}`,
+//             "Content-Type": "application/json",
+//           },
+//         }
+//       );
+//       const data = await response.json();
+//       dispatch(setFriends({ friends: data }));
+//     } catch (error) {
+//       console.error("Error adding friend:", error);
+//     }
+//   };
 
-  return friends.map((friend) => (
-    <FlexBetween key={friend._id}>
-      <Box height="2rem" py=".5rem" display="flex" alignItems="center">
-        <UserImg imgURL={friend.picturePath} />
-        <Box pl=".8rem" fontSize={".9rem"}>
-          {`${friend.firstName} ${friend.lastName}`}
-          <Box fontSize={".8rem"} color="grey">
-            {friend.occupation}
-          </Box>
-        </Box>
-      </Box>
-      <PersonRemoveIcon
-        sx={{
-          background: "hsl(200, 53%, 90%)",
-          color: "hsl(210, 80%, 65%)",
-          fontSize: ".8rem",
-          padding: "5px",
-          boxShadow: "5px",
-          borderRadius: "1rem",
-          cursor: "pointer",
-        }}
-        onClick={() => addRemoveFriend(friend._id)}
-      />
-    </FlexBetween>
-  ));
-};
+//   return friends.map((friend) => (
+//     <FlexBetween key={friend._id}>
+//       <Box height="2rem" py=".5rem" display="flex" alignItems="center">
+//         <UserImg imgURL={friend.picturePath} />
+//         <Box pl=".8rem" fontSize={".9rem"}>
+//           {`${friend.firstName} ${friend.lastName}`}
+//           <Box fontSize={".8rem"} color="grey">
+//             {friend.occupation}
+//           </Box>
+//         </Box>
+//       </Box>
+//       <PersonRemoveIcon
+//         sx={{
+//           background: "hsl(200, 53%, 90%)",
+//           color: "hsl(210, 80%, 65%)",
+//           fontSize: ".8rem",
+//           padding: "5px",
+//           boxShadow: "5px",
+//           borderRadius: "1rem",
+//           cursor: "pointer",
+//         }}
+//         onClick={() => addRemoveFriend(friend._id)}
+//       />
+//     </FlexBetween>
+//   ));
+// };
 
 export default FriendRow;
